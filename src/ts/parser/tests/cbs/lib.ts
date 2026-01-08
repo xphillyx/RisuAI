cb33e5b670ec89a6b8f6ee91eac197ab9af70ab2
enzi221 <enz3224@gmail.com>
2026-01-09 04:11:12 +0900

    test: Move validCBSArgProp into lib.ts

--- src/ts/parser/tests/cbs/lib.ts
+++ src/ts/parser/tests/cbs/lib.ts
@@ -1,3 +1,8 @@
+import fc from 'fast-check'
+
+export const cbs = (op: string, ...args: string[]): string =>
+  `{{${op}${args.length > 0 ? '::' : ''}${args.join('::')}}}`
+
 export const trimVarPrefix = (key: unknown): string => {
   if (typeof key !== 'string') {
     return 'null'
@@ -13,3 +18,11 @@ export const trimVarPrefix = (key: unknown): string => {
 
   return 'null'
 }
+
+/** No hashes, colons, curly braces, line breaks */
+export const validCBSArgRegExp = /^[^#:{}\r\n]+$/
+/** {@link validCBSArgRegExp `validCBSArgRegExp`} */
+export const validCBSArgProp = fc.oneof(
+  fc.stringMatching(validCBSArgRegExp),
+  fc.string({ unit: 'grapheme' }).filter((s) => validCBSArgRegExp.test(s))
+)

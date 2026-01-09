c137c688e6ad4e9785c7eb6e590d52d4ac04b399
enzi221 <enz3224@gmail.com>
2026-01-10 01:42:08 +0900

    refactor: Move parseChatML out of parser.svelte.ts

--- src/ts/parser.svelte.ts
+++ src/ts/parser.svelte.ts
@@ -12,7 +12,6 @@ import { calcString } from './process/infunctions';
 import { findCharacterbyId, getPersonaPrompt, getUserIcon, getUserName, parseKeyValue, pickHashRand, replaceAsync} from './util';
 import { getInlayAssetBlob } from './process/files/inlays';
 import { getModuleAssets, getModuleLorebooks, getModules } from './process/modules';
-import type { OpenAIChat } from './process/index.svelte';
 import hljs from 'highlight.js/lib/core'
 import 'highlight.js/styles/atom-one-dark.min.css'
 import { language } from 'src/lang';
@@ -1926,64 +1925,4 @@ export function applyMarkdownToNode(node: Node) {
             applyMarkdownToNode(child);
         }
     }
-}
-
-export function parseChatML(data:string):OpenAIChat[]|null{
-
-    const starter = '<|im_start|>'
-    const seperator = '<|im_sep|>'
-    const ender = '<|im_end|>'
-    const trimedData = data.trim()
-    if(!trimedData.startsWith(starter)){
-        return null
-    }
-
-    return trimedData.split(starter).filter((f) => f !== '').map((v) => {
-        let role:'system'|'user'|'assistant' = 'user'
-        //default separators
-        if(v.startsWith('user' + seperator)){
-            role = 'user'
-            v = v.substring(4 + seperator.length)
-        }
-        else if(v.startsWith('system' + seperator)){
-            role = 'system'
-            v = v.substring(6 + seperator.length)
-        }
-        else if(v.startsWith('assistant' + seperator)){
-            role = 'assistant'
-            v = v.substring(9 + seperator.length)
-        }
-        //space/newline separators
-        else if(v.startsWith('user ') || v.startsWith('user\n')){
-            role = 'user'
-            v = v.substring(5)
-        }
-        else if(v.startsWith('system ') || v.startsWith('system\n')){
-            role = 'system'
-            v = v.substring(7)
-        }
-        else if(v.startsWith('assistant ') || v.startsWith('assistant\n')){
-            role = 'assistant'
-            v = v.substring(10)
-        }
-
-        v = v.trim()
-
-        if(v.endsWith(ender)){
-            v = v.substring(0, v.length - ender.length)
-        }
-
-
-        let thoughts:string[] = []
-        v = v.replace(/<Thoughts>(.+)<\/Thoughts>/gms, (match, p1) => {
-            thoughts.push(p1)
-            return ''
-        })
-
-        return {
-            role: role,
-            content: risuChatParser(v),
-            thoughts: thoughts
-        }
-    })
 }

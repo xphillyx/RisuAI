82ecb3625e8bdfc9da6bb4dba948112d0b071e21
kwaroran <amasio1234@proton.me>
2026-01-16 23:47:18 +0900

    feat: add name props from Iconbutton and add missing prop rune

--- src/lib/UI/GUI/IconButton.svelte
+++ src/lib/UI/GUI/IconButton.svelte
@@ -6,9 +6,16 @@
     interface Props {
         children: Snippet;
         onclick: (e: MouseEvent) => void;
+        name?: string
     }
+
+    const { children, onclick, name }: Props = $props();
 </script>
 
 <button class="flex items-center hover:text-blue-500 transition-colors" onclick={onclick}>
     {@render children?.()}
+
+    {#if name}
+        <span class="ml-1">{name}</span>
+    {/if}
 </button>

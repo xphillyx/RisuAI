68b4fe6ca6d181d6d6504202eb536a7b890d9475
kwaroran <amasio1234@proton.me>
2026-01-16 23:45:28 +0900

    feat: add IconButton component for reusable button functionality

---
+++ src/lib/UI/GUI/IconButton.svelte
@@ -0,0 +1,14 @@
+
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+
+    interface Props {
+        children: Snippet;
+        onclick: (e: MouseEvent) => void;
+    }
+</script>
+
+<button class="flex items-center hover:text-blue-500 transition-colors button-icon-copy" onclick={onclick}>
+    {@render children?.()}
+</button>

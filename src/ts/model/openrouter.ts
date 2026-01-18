7f6fd19240763a9f3753d3d02ad433223b6ec67c
enzi221 <enz3224@gmail.com>
2026-01-19 03:01:03 +0900

    feat: Improve OpenRouter experience

--- src/ts/model/openrouter.ts
+++ src/ts/model/openrouter.ts
@@ -1,21 +1,51 @@
 import { getDatabase } from "../storage/database.svelte"
 
-export async function openRouterModels() {
+export async function getOpenRouterProviders(): Promise<{ name: string, slug: string }[]> {
     try {
         const db = getDatabase()
-        let headers = {
+        const headers = {
             "Authorization": "Bearer " + db.openrouterKey,
             "Content-Type": "application/json"
         }
 
-        const aim = fetch("https://openrouter.ai/api/v1/models", {
-            headers: headers
-        })  
-        const res = await (await aim).json()
-        return res.data.map((model: any) => {
+        const providers: { data: { name: string, slug: string }[] } = await fetch("https://openrouter.ai/api/v1/providers", {
+            headers,
+        }).then((res) => res.json())
+
+        return providers.data.map(({ name, slug }) => ( { name, slug })).sort((a, b) => a.name.localeCompare(b.name))
+    } catch (error) {
+        return []
+    }
+}
+
+type OpenRouterModel = {
+    canonical_slug: string
+    context_length: number
+    id: string
+    name: string
+    pricing: {
+        prompt: string,
+        completion: string,
+        web_search: string,
+        input_cache_read: string
+    }
+}
+
+export async function getOpenRouterModels(): Promise<OpenRouterModel[]> {
+    try {
+        const db = getDatabase()
+        const headers = {
+            "Authorization": "Bearer " + db.openrouterKey,
+            "Content-Type": "application/json"
+        }
+
+        const aim = await fetch("https://openrouter.ai/api/v1/models", {
+            headers,
+        }).then((res) => res.json())
+
+        return aim.data.map((model: any) => {
             let name = model.name
             let price = ((Number(model.pricing.prompt) * 3) + Number(model.pricing.completion)) / 4
-            console.log(model.pricing, price)
             if(price > 0){
                 name += ` - $${(price*1000).toFixed(5)}/1k`
             }
@@ -28,18 +58,18 @@ export async function openRouterModels() {
                 price: price,
                 context_length: model.context_length,
             }
-        }).sort((a: any, b: any) => {
-            return a.price - b.price
         }).filter((model: any) => {
             return model.price >= 0
+        }).sort((a: any, b: any) => {
+            return a.price - b.price
         })
     } catch (error) {
         return []
     }
 }
 
-export async function getFreeOpenRouterModel(){
-    const models = await openRouterModels()
+export async function getFreeOpenRouterModels(){
+    const models = await getOpenRouterModels()
     return models.filter((model: any) => {
         return model.name.endsWith("Free")
     }).sort((a: any, b: any) => {

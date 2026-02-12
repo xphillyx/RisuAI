854f2a33eab3cf426c4ad7b7d94ddc0bd26b1c0b
enzi221 <enz3224@gmail.com>
2026-02-12 21:17:16 +0900

    chore: Remove unused imports in google.ts

--- src/ts/process/request/google.ts
+++ src/ts/process/request/google.ts
@@ -1,5 +1,4 @@
 import { fetchNative, textifyReadableStream } from "src/ts/globalApi.svelte"
-import { language } from "src/lang"
 import { LLMFlags, LLMFormat } from "src/ts/model/modellist"
 import { getDatabase, setDatabase } from "src/ts/storage/database.svelte"
 import { simplifySchema } from "src/ts/util"
@@ -7,7 +6,7 @@ import { v4 } from "uuid"
 import { setInlayAsset, writeInlayImage } from "../files/inlays"
 import { extractJSON, getGeneralJSONSchema } from "../templates/jsonSchema"
 import { callTool, decodeToolCall, encodeToolCall } from "../mcp/mcp"
-import { alertError, alertNormal, alertWait, showHypaV2Alert } from "src/ts/alert";
+import { alertError } from "src/ts/alert";
 import { addFetchLog } from "src/ts/globalApi.svelte"
 import type { RequestDataArgumentExtended, requestDataResponse, StreamResponseChunk } from './request'
 import { applyParameters, type Parameter } from './shared'

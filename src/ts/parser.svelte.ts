5ae0ba891f91f9f39d1dda51d38fb90e2ced4333
kwaroran <amasio1234@proton.me>
2026-02-13 21:22:57 +0900

    fix: typo in blockquoteStyling

--- src/ts/parser.svelte.ts
+++ src/ts/parser.svelte.ts
@@ -192,7 +192,7 @@ function renderMarkdown(md:markdownit, data:string){
 
         //clean up any unmatched quote marks
         text = text.replace(/\uE9b0/gu,  quotes[0]).replace(/\uE9b1/gu, quotes[1])
-        text = text.replace(/\uE9b2/gu, quotes[2]).replace(/\uE9b3/gu, quotes[3])
+        
     }
     else{
         text = text.replace(/\uE9b0/gu, '<mark risu-mark="quote2">' + quotes[0]).replace(/\uE9b1/gu, quotes[1] + '</mark>')

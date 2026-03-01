ba4b71a80ac6df639188cdf14646db0c702af137
kwaroran <amasio1234@proton.me>
2026-03-01 23:14:33 +0900

    fix: enhance CSP by adding default-src directive in SandboxHost class

--- src/ts/plugins/apiV3/factory.ts
+++ src/ts/plugins/apiV3/factory.ts
@@ -266,7 +266,7 @@ export class SandboxHost {
     private iframe: HTMLIFrameElement;
     private apiFactory: any;
     private nonce = crypto.randomUUID();
-    private csp = `connect-src 'none'; script-src 'nonce-${this.nonce}' https:; frame-src 'none'; object-src 'none'; style-src * 'unsafe-inline';`;
+    private csp = `connect-src 'none'; script-src 'nonce-${this.nonce}' https:; frame-src 'none'; object-src 'none'; style-src * 'unsafe-inline'; default-src 'none';`;
 
     private instanceRegistry = new Map<string, any>();
 
